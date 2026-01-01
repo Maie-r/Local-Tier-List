9de9a8e4f77a0ec2096aa796bd8a745d15134ce3
Maie-r <nitainandi1@gmail.com>
2026-01-01 20:04:26 -0300

    Minor Mobile visual ajustments; not functional

--- Local Tier List/Data/TierService.cs
+++ Local Tier List/Data/TierService.cs
@@ -9,12 +9,27 @@ namespace Local_Tier_List.Data
 {
     public class TierService
     {
-        private static TierLister _tierlister = new TierLister();
+        private static TierLister _tierlister;
 
-        public static TierLister tl { get { return _tierlister; } }
+        public static TierLister tl 
+        { 
+            get 
+            { 
+                if (_tierlister == null) 
+                { 
+                    _tierlister = new TierLister(); 
+                }
+                return _tierlister; 
+            } 
+        }
 
         public static List<TierList> TierLists 
-        { get { return _tierlister.TierLists; } }
+        { 
+            get 
+            {
+                return tl.TierLists; 
+            } 
+        }
 
         public static void SaveAll()
         {
