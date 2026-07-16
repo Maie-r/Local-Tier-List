584b4ba017eca87221411c15bc009360f5038cb3
Maie- <nitainandi1@gmail.com>
2026-07-16 18:52:17 -0300

    Rewrote modals to their own components. Added a loading display for more heavy components.

---
+++ Local Tier List/Data/App/CommonEnums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Local_Tier_List.Data.App
+{
+    public enum TLListModalType
+    {
+        TLEdit,
+        FolderEdit,
+        TLDelete,
+        FolderDelete,
+        None
+    }
+
+    public enum IOModalType
+    {
+        Import,
+        Export,
+        Merge,
+        None,
+    }
+}
